e310ed0045f582d028d4f9cc3e4fc199e90e43da
Antony33304 <axew-863@hotmail.com>
2024-11-12 20:46:34 -0600

    Actualizacion en laclase poliza
    Se agregaron los atributos clavePolza, tipoSeguro, montoAsegurado, descripcion, Cliente.
    Se creo un contructor vacio y se generaron el constructor y los geters correspondientes.

--- proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/modelo/Poliza.java
+++ proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/modelo/Poliza.java
@@ -1,5 +1,40 @@
 package mx.edu.uacm.is.slt.as.ws.modelo;
 
 public class Poliza {
+	
+	private String clavePoliza;
+	private String tipoSeguro;
+	private double montoAsegurado;
+	private String descripcion;
+	private Cliente cliente;
+	
+	public Poliza(String clavePoliza, String tipoSeguro, double montoAsegurado, String descripcion, Cliente cliente) {
+		
+		this.clavePoliza = clavePoliza;
+		this.tipoSeguro = tipoSeguro;
+		this.montoAsegurado = montoAsegurado;
+		this.descripcion = descripcion;
+		this.cliente = cliente;
+	}
+
+	public String getClavePoliza() {
+		return clavePoliza;
+	}
+
+	public String getTipoSeguro() {
+		return tipoSeguro;
+	}
+
+	public double getMontoAsegurado() {
+		return montoAsegurado;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
 
 }
