dab9162e4a62f10cbc8219ae6a400655c2d4b704
jesfree <jshn45@gmail.com>
2024-11-15 21:48:21 -0600

    creacion del metodo post para revision y testeo enla rama original

--- proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/controlador/PolizaControlador.java
+++ proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/controlador/PolizaControlador.java
@@ -2,15 +2,30 @@ package mx.edu.uacm.is.slt.as.ws.controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import mx.edu.uacm.is.slt.as.ws.modelo.Poliza;
 
 @RestController
 @RequestMapping("/api/polizas")
 public class PolizaControlador {
 	
+	
+	@PostMapping("/cliente/{curp}")					//post --> insertar 
+	public String crearPoliza(@RequestBody Poliza poliza ) {
+		return "Curp cliente:" + poliza;
+	}
+	
+		
+	
 	@GetMapping("/{id}")
 	public String obtenerPoliza(@PathVariable("id") Long id) {
 		return "Poliza con ID: "+ id;
 	}
+	
+	
+	
 }
