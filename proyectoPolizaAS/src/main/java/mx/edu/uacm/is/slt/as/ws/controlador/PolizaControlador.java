ce3885238554c7c0c3220a4a601e73c56980d3dd
AbrahamBungie <sal.abraham2907@outlok.com>
2024-11-20 12:55:20 -0600

    Se arregló un bug con el propiertes en el campo password

--- proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/controlador/PolizaControlador.java
+++ proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/controlador/PolizaControlador.java
@@ -18,28 +18,23 @@ import mx.edu.uacm.is.slt.as.ws.modelo.Poliza;
 @RequestMapping("/api/polizas")
 public class PolizaControlador {
 	
-	
 	@PostMapping("/cliente/{curp}")					//post --> insertar 
 	public String crearPoliza(@RequestBody Poliza poliza ) {
 		return "Curp cliente:" + poliza;
 	}
-	
-		
 	
 	@GetMapping("/{id}")
 	public String obtenerPoliza(@PathVariable("id") Long id) {
 		return "Poliza con ID: "+ id;
 	}
 	
-
 	@PutMapping("/{id}")//Se agrego metodo put
 	public String actualizarPoliza(@PathVariable("id") Long id, @RequestBody String poliza) {
 		return "\n\nPoliza con ID: " + id + " actualizada a: " + poliza +"\n\n";
 	}
-	
+
 	@DeleteMapping("/{id}")//metodo delete
 	public String eliminarPoliza(@PathVariable("id") Long id) {
 		return "La Poliza con ID: " + id + "ha sido eliminada";
 	}
-
 }
