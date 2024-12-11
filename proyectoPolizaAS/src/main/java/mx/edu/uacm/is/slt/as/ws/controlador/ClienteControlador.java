c27e63194a3056f026626944a1471bbd5c1b8c65
jesfree <jshn45@gmail.com>
2024-12-11 00:35:59 -0600

    metodo implementado que devuelve cliente por nombre ya con pruebas hechas

--- proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/controlador/ClienteControlador.java
+++ proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/controlador/ClienteControlador.java
@@ -2,8 +2,10 @@ package mx.edu.uacm.is.slt.as.ws.controlador;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,4 +43,32 @@ public class ClienteControlador {
         
         return cliente;
     }
+	
+	@GetMapping("/polizas/c/{nombre}/{primer_apellido}/{segundo_apellido}")
+	public List<Cliente> obtenerClientesPorNombre(@PathVariable String nombre, 
+	                                              @PathVariable String primer_apellido, 
+	                                              @PathVariable(required = false) String segundo_apellido) {
+	    // Buscar clientes por nombre y primer apellido
+	    List<Cliente> clientes;
+	    if (segundo_apellido != null) {
+	        // Si se proporciona segundo apellido, buscar por todos los apellidos
+	        clientes = clienteRepository.findByNombreAndPrimerApellidoAndSegundoApellido(nombre, primer_apellido, segundo_apellido);
+	    } else {
+	        // Si no se proporciona segundo apellido, buscar solo por nombre y primer apellido
+	        clientes = clienteRepository.findByNombreAndPrimerApellidoAndSegundoApellido(nombre, primer_apellido, null);
+	        System.out.println("No se encontraron los cliente con sólo 1 apellido");
+	    }
+	    
+	    // Si no se encuentran clientes, lanzar excepción
+	    if (clientes.isEmpty()) {
+	        System.out.println("No se encontraron clientes con los datos proporcionados.");
+	    } else {
+	    	 // Si se encuentran clientes, imprimir mensaje de éxito
+	        System.out.println("Se encontraron " + clientes.size() + " clientes con los datos proporcionados.");
+	    }
+	    
+	    return clientes;
+	}
+	
+	
 }
--- proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/repository/ClienteRepository.java
+++ proyectoPolizaAS/src/main/java/mx/edu/uacm/is/slt/as/ws/repository/ClienteRepository.java
@@ -1,5 +1,6 @@
 package mx.edu.uacm.is.slt.as.ws.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +12,7 @@ import mx.edu.uacm.is.slt.as.ws.modelo.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, String>{
 	Optional<Cliente> findByCurp(String curp);
+	
+	List<Cliente> findByNombreAndPrimerApellidoAndSegundoApellido(String nombre, String primerApellido, String segundoApellido);
 }
  
